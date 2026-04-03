3e36eeed10d6c791f29007c69559173572cbe38d
TeyzG <TeyzG.L@gmail.com>
2026-04-03 10:03:17 +0700

    update database

--- init.sql
+++ init.sql
@@ -2,4 +2,4 @@ CREATE TABLE IF NOT EXISTS tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL
 );
-INSERT INTO tasks (title) VALUES ('Học Docker'), ('Làm bài tập DevOps');
+INSERT INTO tasks (title) VALUES ('Hoc Docker'), ('Lam bai tap DevOps');
